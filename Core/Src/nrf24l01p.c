fb8b2bece1d15315740f6a51dbb36783ae7633cd
Devin Rataiczak <rataiczak.devin@gmail.com>
2024-04-29 20:55:20 -0400

    Initial implementation for reg getters/setters

--- Core/Src/nrf24l01p.c
+++ Core/Src/nrf24l01p.c
@@ -123,121 +123,237 @@ void nRF24_FlushRx(nrf24_t *nrf) {
 }
 
 uint8_t nRF24_GetConfig(nrf24_t *nrf) {
-    
+    return nRF24_ReadReg(nrf, CONFIG_REG);
 }
 
 uint8_t nRF24_GetAutoAck(nrf24_t *nrf) {
-
+    return nRF24_ReadReg(nrf, EN_AA_REG);
 }
 
 uint8_t nRF24_GetEnabledRxAddrs(nrf24_t *nrf) {
-    
+    return nRF24_ReadReg(nrf, EN_RXADDR_REG);
 }
 
 uint8_t nRF24_GetAddrWidth(nrf24_t *nrf) {
-    
+    return nRF24_ReadReg(nrf, SETUP_AW_REG);
 }
 
 uint8_t nRF24_GetAutoRetransmit(nrf24_t *nrf) {
-    
+    return nRF24_ReadReg(nrf, SETUP_RETR_REG); 
 }
 
 uint8_t nRF24_GetRFChannel(nrf24_t *nrf) {
-    
+    return nRF24_ReadReg(nrf, RF_CH_REG);
 }
 
 uint8_t nRF24_GetRFConfig(nrf24_t *nrf) {
-    
+    return nRF24_ReadReg(nrf, RF_SETUP_REG);
 }
 
 uint8_t nRF24_GetStatus(nrf24_t *nrf) {
-    uint8_t status;
-
-    status = nRF24_ReadReg(nrf, CONFIG_REG);
-
-    return status;
+    return nRF24_ReadReg(nrf, STATUS_REG);
 }
 
 uint8_t nRF24_GetTxObserve(nrf24_t *nrf) {
-    
+    return nRF24_ReadReg(nrf, OBSERVE_TX_REG);
 }
 
 uint8_t nRF24_GetRPD(nrf24_t *nrf) {
-    
+    return nRF24_ReadReg(nrf, RPD_REG);
 }
 
 void nRF24_GetRXPipeAddr(nrf24_t *nrf, pipe_t pipe, uint8_t *buf, size_t size) {
+    uint8_t addr;
     
+    switch(pipe) {
+        case p0:
+            addr = RX_ADDR_P0_REG;
+            break;
+        case p1:
+            addr = RX_ADDR_P1_REG;
+            break;
+        case p2:
+            addr = RX_ADDR_P2_REG;
+            break;
+        case p3:
+            addr = RX_ADDR_P3_REG;
+            break;
+        case p4:
+            addr = RX_ADDR_P4_REG;
+            break;
+        case p5:
+            addr = RX_ADDR_P5_REG;
+            break;
+        default:
+            return;
+    }
+    
+    nRF24_ReadMBReg(nrf, addr, buf, size);
 }
 
 void nRF24_GetTXAddr(nrf24_t *nrf, uint8_t *buf, size_t size) {
-    
+    nRF24_ReadMBReg(nrf, TX_ADDR_REG, buf, size);
 }
 
 uint8_t nRF24_GetRXPipeWidth(nrf24_t *nrf, pipe_t pipe) {
-    
+    uint8_t addr;
+
+    switch(pipe) {
+        case p0:
+            addr = RX_PW_P0_REG;
+            break;
+        case p1:
+            addr = RX_PW_P1_REG;
+            break;
+        case p2:
+            addr = RX_PW_P2_REG;
+            break;
+        case p3:
+            addr = RX_PW_P3_REG;
+            break;
+        case p4:
+            addr = RX_PW_P4_REG;
+            break;
+        case p5:
+            addr = RX_PW_P5_REG;
+            break;
+        default:
+            return;
+    }
+
+    return nRF24_ReadReg(nrf, addr);
 }
 
 uint8_t nRF24_GetFifoStatus(nrf24_t *nrf) {
-    
+    return nRF24_ReadReg(nrf, FIFO_STATUS_REG);
 }
 
 uint8_t nRF24_GetDynamicPayloadConfig(nrf24_t *nrf) {
-    
+    return nRF24_ReadReg(nrf, DYNPD_REG);
 }
 
 uint8_t nRF24_GetFeatureConfig(nrf24_t *nrf) {
-
+    return nRF24_ReadReg(nrf, FEATURE_REG);
 }
 
 void nRF24_SetConfig(nrf24_t *nrf, uint8_t value) {
     value &= MASK_CONFIG;
+
+    nRF24_WriteReg(nrf, CONFIG_REG, value);
 }
 
 void nRF24_SetAutoAck(nrf24_t *nrf, uint8_t value) {
     value &= MASK_AUTOACK;
+
+    nRF24_WriteReg(nrf, EN_AA_REG, value);
 }
 
 void nRF24_SetEnabledRxAddrs(nrf24_t *nrf, uint8_t value) {
     value &= MASK_ENABLERX_ADDR;
+
+    nRF24_WriteReg(nrf, EN_RXADDR_REG, value);
 }
 
 void nRF24_SetAddrWidth(nrf24_t *nrf, uint8_t value) {
     value &= MASK_ADDR_WIDTH;
+
+    nRF24_WriteReg(nrf, SETUP_AW_REG, value);
 }
 
 void nRF24_SetAutoRetransmit(nrf24_t *nrf, uint8_t value) {
-
+    nRF24_WriteReg(nrf, SETUP_RETR_REG, value);
 }
 
 void nRF24_SetRFChannel(nrf24_t *nrf, uint8_t value) {
     value &= MASK_RF_CH;
+
+    nRF24_WriteReg(nrf, RF_CH_REG, value);
 }
 
 void nRF24_SetRFConfig(nrf24_t *nrf, uint8_t value) {
     value &= MASK_RF_CONFIG;
+
+    nRF24_WriteReg(nrf, RF_SETUP_REG, value);
 }
 
 void nRF24_SetStatus(nrf24_t *nrf, uint8_t value) {
     value &= MASK_STATUS;
+
+    nRF24_WriteReg(nrf, STATUS_REG, value);
 }
 
 void nRF24_SetRXPipeAddr(nrf24_t *nrf, pipe_t pipe, uint8_t *buf, size_t size){
-    
+    uint8_t addr;
+
+    switch(pipe) {
+        case p0:
+            addr = RX_ADDR_P0_REG;
+            break;
+        case p1:
+            addr = RX_ADDR_P1_REG;
+            break;
+        case p2:
+            addr = RX_ADDR_P2_REG;
+            break;
+        case p3:
+            addr = RX_ADDR_P3_REG;
+            break;
+        case p4:
+            addr = RX_ADDR_P4_REG;
+            break;
+        case p5:
+            addr = RX_ADDR_P5_REG;
+            break;
+        default:
+            return;
+    }
+
+    nRF24_WriteMBReg(nrf, addr, buf, size);
 }
 
 void nRF24_SetTXAddr(nrf24_t *nrf, uint8_t *buf, size_t size){
-
+    nRF24_WriteMBReg(nrf, TX_ADDR_REG, buf, size);
 }
 
 void nRF24_SetRXPipeWidth(nrf24_t *nrf, pipe_t pipe, uint8_t value){
+    uint8_t addr;
+
     value &= MASK_RX_PW_PIPE;
+
+    switch(pipe) {
+        case p0:
+            addr = RX_PW_P0_REG;
+            break;
+        case p1:
+            addr = RX_PW_P1_REG;
+            break;
+        case p2:
+            addr = RX_PW_P2_REG;
+            break;
+        case p3:
+            addr = RX_PW_P3_REG;
+            break;
+        case p4:
+            addr = RX_PW_P4_REG;
+            break;
+        case p5:
+            addr = RX_PW_P5_REG;
+            break;
+        default:
+            return;
+    }
+
+    nRF24_WriteReg(nrf, addr, value);
 }
 
 void nRF24_SetDynamicPayloadConfig(nrf24_t *nrf, uint8_t value){
     value &= MASK_DYNPD;
+
+    nRF24_WriteReg(nrf, DYNPD_REG, value);
 }
 
 void nRF24_SetFeatureConfig(nrf24_t *nrf, uint8_t value){
     value &= MASK_FEATURE;
+
+    nRF24_WriteReg(nrf, FEATURE_REG, value);
 }
